e6cc8c7fe5611db1f88413e73b1e90e3a7f4a58a
Kelson Warner <kelson.warner@fundingcircle.com>
2020-03-07 16:15:22 -0800

    add menu item search resolver

--- lib/plateslate_web/schema.ex
+++ lib/plateslate_web/schema.ex
@@ -1,17 +1,27 @@
 defmodule PlateslateWeb.Schema do
   use Absinthe.Schema
   alias Plateslate.{Menu, Repo}
+  alias PlateslateWeb.Resolvers
 
   query do
     @desc "The list of available Menu Items!"
     field :menu_items, list_of(:menu_item) do
-      resolve(fn _, _, _ -> {:ok, Repo.all(Menu.Item)} end)
+      arg(:matching, :string)
+      resolve(&Resolvers.Menu.menu_items/3)
     end
   end
 
+  @desc "A single available MenuItem"
   object :menu_item do
+    @desc "The MenuItem's ID"
     field :id, :id
+    @desc "The MenuItem's name"
     field :name, :string
+    @desc "The MenuItem's optional description"
     field :description, :string
+    @desc "The MenuItem's price"
+    field :price, :float
+    @desc "The MenuItem's added_on date"
+    field :added_on, :string
   end
 end
